77952865b548d1bd4ca22da47fe47607177094fb
hahaha <a310608318@gmail.com>
2025-03-08 23:21:57 +0800

    3.8

--- test2.cpp
+++ test2.cpp
@@ -9,140 +9,28 @@
 
 using namespace std;
 
-const int MAX_DEPTH = 6;
-const int SIZE = 100000000;
-
-int count = 0;
-
-vector<int> arr;
-
-class Arr
-{
-public:
-	Arr(vector<int> &a, int l, int h, int d) : array(a), low(l), high(h), depth(d) {}
-
-	vector<int> &array;
-	int low;
-	int high;
-	int depth = 0;
-};
-
-int partition(vector<int> &array, int low, int high)
-{
-
-	int pivot = array[low];
-	while (low < high)
-	{
-		while (low < high && array[high] >= pivot)
-			high--;
-		array[low] = array[high];
-
-		while (low < high && array[low] <= pivot)
-			low++;
-		array[high] = array[low];
-	}
-	array[low] = pivot;
-	return low;
-}
-
-void quicksort(vector<int> &array, int low, int high)
-{
-	if (low < high)
-	{
-		int pivot = partition(array, low, high);
-		quicksort(array, low, pivot - 1);
-		quicksort(array, pivot + 1, high);
-	}
-}
-
-void quicksort_multiple(Arr &arr)
-{
-	if (arr.depth < MAX_DEPTH)
-	{
-		int pivot = partition(arr.array, arr.low, arr.high);
-
-		Arr arrleft(arr.array, arr.low, pivot - 1, arr.depth + 1);
-		Arr arrright(arr.array, pivot + 1, arr.high, arr.depth + 1);
-
-		thread left(quicksort_multiple, ref(arrleft));
-		thread right(quicksort_multiple, ref(arrright));
-
-		left.join();
-		right.join();
-	}
-	else
-		quicksort(arr.array, arr.low, arr.high);
-}
-
-void quicksort_multiple2(vector<int> &array, int low, int high, int depth)
-{
-	if (depth < MAX_DEPTH)
-	{
-		int pivot = partition(array, low, high);
-
-		thread right(quicksort_multiple2, ref(array), pivot + 1, high, depth + 1);
-		// quicksort_multiple2(array, low, pivot - 1, depth + 1);
-		thread left(quicksort_multiple2, ref(array), low, pivot - 1, depth + 1);
-
-		left.join();
-		right.join();
-	}
-	else
-		quicksort(array, low, high);
-
-	return;
-}
-
-void *quicksort_multiple_byC(void *arg)
-{
-	Arr *tmp = (Arr *)arg;
-	if (tmp->depth < MAX_DEPTH)
-	{
-		if (tmp->low < tmp->high)
-		{
-			int pivot = partition(tmp->array, tmp->low, tmp->high);
-
-			Arr arrleft(tmp->array, tmp->low, pivot - 1, tmp->depth + 1);
-			Arr arrright(tmp->array, pivot + 1, tmp->high, tmp->depth + 1);
-			pthread_t left, right;
-			int ret;
-			ret = pthread_create(&left, NULL, quicksort_multiple_byC, &arrleft);
-			if (ret != 0)
-				fprintf(stderr, "pthread_create:%s\n", strerror(ret));
-
-			ret = pthread_create(&right, NULL, quicksort_multiple_byC, &arrright);
-			if (ret != 0)
-				fprintf(stderr, "pthread_create:%s\n", strerror(ret));
-
-			ret = pthread_join(left, NULL);
-			if (ret != 0)
-				fprintf(stderr, "pthread_create:%s\n", strerror(ret));
-			ret = pthread_join(right, NULL);
-			if (ret != 0)
-				fprintf(stderr, "pthread_create:%s\n", strerror(ret));
+vector<int> cal_num(int n){
+	vector<int> result(1,1);
+	for(int i=2;i<=n;i++){
+		int carry=0;
+		for(int j=0;j<result.size();j++){
+			int temp=result[j]*i+carry;
+			result[j]=temp%10;
+			carry=temp/10;
+		}
+		while(carry){
+			result.push_back(carry%10);
+			carry/=10;
 		}
 	}
-	else
-		quicksort(tmp->array, tmp->low, tmp->high);
-	return NULL;
+	return result;
 }
-
-int main()
-{
-	srand(time(NULL));
-
-	arr.reserve(SIZE);
-	for (int i = 0; i < SIZE; i++)
-		arr.push_back(rand());
-
-	Arr origin(arr, 0, SIZE - 1, 0);
-
-	cout << "start" << endl;
-	quicksort_multiple(origin);
-	// quicksort_multiple2(arr, 0, SIZE - 1, 0);
-	//   quicksort_multiple_byC(&origin);
-
-	// for (int i = 0; i < 100000000; i++)
-	//	cout << arr[i];
+int main(){
+	vector<int> result=cal_num(100);
+	int start = result.size() - 1;
+    for (int i = result.size() - 1; i >= 0; i--) {
+        cout << result[i];
+    }
+    cout << endl;
 	return 0;
 }
--- thread_pool.cpp
+++ thread_pool.cpp
@@ -1,5 +1,7 @@
 #include <cmath>
+#include <future>
 #include <iostream>
+#include <memory>
 #include <pthread.h>
 #include <unistd.h>
 #include <vector>
@@ -7,28 +9,28 @@
 #include <functional>
 using namespace std;
 const int MAX_NUM=10;
-queue<function<void()>> workqueue;
 
-// pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-// pthread_cond_t  cond_work=PTHREAD_COND_INITIALIZER;
-// pthread_cond_t  cond_finish=PTHREAD_COND_INITIALIZER;
 
 class thread_pool{
     public:
+    queue<function<void()>> workqueue;
     bool stop;
-    vector<int> working;
     vector<pthread_t> pthread_num;
     pthread_mutex_t mutex ;
     pthread_cond_t  cond_work;
     public:
     thread_pool(){
-        working.reserve(MAX_NUM);
-        pthread_num.reserve(MAX_NUM);
+        pthread_mutex_init(&mutex,NULL);
+        pthread_cond_init(&cond_work, NULL);
+        pthread_num.resize(MAX_NUM);
         stop=false;
         for(int i=0;i<MAX_NUM;i++){
-            pthread_create(&pthread_num[i],NULL,
+            if(pthread_create(&pthread_num[i],NULL,
             [](void*arg)->void*{static_cast<thread_pool*>(arg)->work();
-            return NULL;},this);
+            return NULL;},this)!=0){
+                cout<<"thread create failed"<<endl;
+                exit(-1);
+            }
         }
     }
     public:
@@ -38,39 +40,72 @@ class thread_pool{
         for(int i=0;i<MAX_NUM;i++){
             pthread_join(pthread_num[i],NULL);
         }
+        pthread_mutex_destroy(&mutex);
+        pthread_cond_destroy(&cond_work);
     }
     public:
     void work(){
-        while(!stop){
+        while(true){
             pthread_mutex_lock(&mutex);
-            while(workqueue.empty()){
+            function<void()> task;
+            while(workqueue.empty()&&!stop){
                 pthread_cond_wait(&cond_work,&mutex);
             }
-            function<void()> haha=workqueue.front();
+            if(stop){
+                    pthread_mutex_unlock(&mutex);
+                    break;
+            }
+            task=std::move(workqueue.front());
             workqueue.pop();
             pthread_mutex_unlock(&mutex);
-            haha();
+            task();
         }
         return ;
     }
     public:
-    void push_task(function<void()> task){
+    template<typename Func>
+    auto push_task(Func func)->future<decltype(func())>{
+
+        using ReturnType=decltype(func());
+
+        auto task=make_shared<packaged_task<ReturnType()>>(std::forward<Func>(func));
+        future<ReturnType> result=task->get_future();
+
         pthread_mutex_lock(&mutex);
-        workqueue.push(task);
+
+        workqueue.push([task](){(*task)();});
+
         pthread_cond_signal(&cond_work);
         pthread_mutex_unlock(&mutex);
+        return result;
     }
 };
 
-
-
-void test(){
-    cout<<"helloworld"<<endl;
+vector<int> cal_num(int n){
+	vector<int> result(1,1);
+	for(int i=2;i<=n;i++){
+		int carry=0;
+		for(int j=0;j<result.size();j++){
+			int temp=result[j]*i+carry;
+			result[j]=temp%10;
+			carry=temp/10;
+		}
+		while(carry){
+			result.push_back(carry%10);
+			carry/=10;
+		}
+	}
+	return result;
 }
 
 int main(){
     thread_pool hahaha;
-    hahaha.push_task(test);
-    hahaha.push_task(test);
+    vector<int> result(1000);
+    future<vector<int>>result1 = hahaha.push_task([](){return cal_num(100);});
+    result=result1.get();
+    for(int i=result.size()-1;i>=0;i--){
+        cout<<result[i];
+    }
+    cout<<endl;
     return 0;  
 }
